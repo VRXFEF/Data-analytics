a0e02185d16fe87da87ac74b8d6652ddd433ffd7
VRXFEF <georgiy.r.s05@gmail.com>
2026-01-20 22:23:42 +1000

    Complete project: added weather data generator, jupyter analytics and dashboards

--- generator/main.py
+++ generator/main.py
@@ -4,12 +4,32 @@ import random
 
 conn = psycopg2.connect(host="db", database="analytics_db", user="user", password="password")
 cur = conn.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS weather (id SERIAL, dt TIMESTAMP DEFAULT CURRENT_TIMESTAMP, temperature FLOAT);")
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS weather (
+    id SERIAL PRIMARY KEY, 
+    dt TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    temperature FLOAT,
+    humidity FLOAT,
+    pressure FLOAT
+);
+""")
 conn.commit()
 
+print("Сервис запущен. Начинаю запись данных...")
+
 while True:
-    temp = round(random.uniform(20, 30), 2)
-    cur.execute("INSERT INTO weather (temperature) VALUES (%s)", (temp,))
+  
+    temp = round(random.uniform(20.0, 30.0), 2)      # Температура 20-30°C
+    hum = round(random.uniform(40.0, 60.0), 2)       # Влажность 40-60%
+    press = round(random.uniform(740.0, 760.0), 1)   # Давление 740-760 мм рт. ст.
+    
+    cur.execute(
+        "INSERT INTO weather (temperature, humidity, pressure) VALUES (%s, %s, %s)", 
+        (temp, hum, press)
+    )
     conn.commit()
-    print(f"Записана температура: {temp}")
+    
+    print(f"Запись: Темп={temp} | Влажн={hum} | Давл={press}")
+    
     time.sleep(5)
